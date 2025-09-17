8a2154fea19f70fcd0b96f7cb3dfbc74f2707632
David Williams <davidw1457@gmail.com>
2025-09-17 11:43:55 -0500

    ch01.l03

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+    "errors"
     "fmt"
+    "io"
+    "log"
     "os"
 )
 
 func main() {
-    fmt.Println("I hope I get the job!")
+    file, err := os.Open("messages.txt")
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    buf := make([]byte, 8)
+    for {
+        _, err = file.Read(buf)
+        if errors.Is(err, io.EOF) {
+            break
+        } else if err != nil {
+            log.Fatalln(err)
+        }
+        fmt.Printf("read: %s\n", string(buf))
+    }
 }
