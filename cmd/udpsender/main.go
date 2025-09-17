b563591c1e869664b4a60140c95f484484fa2591
David Williams <davidw1457@gmail.com>
2025-09-17 14:59:54 -0500

    ch02.l03

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "net"
+    "os"
+)
+
+func main() {
+    u, err := net.ResolveUDPAddr("udp", "localhost:42069")
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    conn, err := net.DialUDP("udp", nil, u)
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    reader := bufio.NewReader(os.Stdin)
+
+    for {
+        fmt.Print(">")
+        input, err := reader.ReadString(byte('\n'))
+        if err != nil {
+            log.Println(err)
+        }
+
+        _, err = conn.Write([]byte(input))
+        if err != nil {
+            log.Println(err)
+        }
+    }
+}
