cddc415901b761aa9340374354fd1c732b05f090
David Williams <davidw1457@gmail.com>
2025-09-17 11:37:02 -0500

    ch01.l02

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/davidw1457/httpfromtcp
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    fmt.Println("I hope I get the job!")
+}
