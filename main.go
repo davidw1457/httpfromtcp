2cc6087393dfd3a438e19ad5a5cecc2f8bd5868a
David Williams <davidw1457@gmail.com>
2025-09-17 12:14:10 -0500

    ch01.l04

--- main.go
+++ main.go
@@ -1,27 +1,37 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "io"
-    "log"
-    "os"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-    file, err := os.Open("messages.txt")
-    if err != nil {
-        log.Fatalln(err)
-    }
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    buf := make([]byte, 8)
-    for {
-        _, err = file.Read(buf)
-        if errors.Is(err, io.EOF) {
-            break
-        } else if err != nil {
-            log.Fatalln(err)
-        }
-        fmt.Printf("read: %s\n", string(buf))
-    }
+	var line string
+
+	for {
+		buf := make([]byte, 8)
+		_, err = file.Read(buf)
+		if errors.Is(err, io.EOF) {
+			break
+		} else if err != nil {
+			log.Fatalln(err)
+		}
+
+		split := strings.Split(string(buf), "\n")
+		line += split[0]
+		for len(split) > 1 {
+			fmt.Printf("read: %s\n", line)
+			split = split[1:]
+			line = split[0]
+		}
+	}
 }
