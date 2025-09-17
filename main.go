77ec5a397073e6c12d235338e872318588de681f
David Williams <davidw1457@gmail.com>
2025-09-17 12:33:13 -0500

    ch01.l05

--- main.go
+++ main.go
@@ -15,23 +15,38 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	var line string
+	c := getLinesChannel(file)
 
-	for {
-		buf := make([]byte, 8)
-		_, err = file.Read(buf)
-		if errors.Is(err, io.EOF) {
-			break
-		} else if err != nil {
-			log.Fatalln(err)
-		}
-
-		split := strings.Split(string(buf), "\n")
-		line += split[0]
-		for len(split) > 1 {
-			fmt.Printf("read: %s\n", line)
-			split = split[1:]
-			line = split[0]
-		}
+	for s := range c {
+		fmt.Printf("read: %s\n", s)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(c)
+		var line string
+		for {
+			buf := make([]byte, 8)
+			_, err := f.Read(buf)
+			if errors.Is(err, io.EOF) {
+				break
+			} else if err != nil {
+				log.Fatalln(err)
+			}
+
+			split := strings.Split(string(buf), "\n")
+			line += split[0]
+			for len(split) > 1 {
+				c <- line
+				split = split[1:]
+				line = split[0]
+			}
+		}
+	}()
+
+	return c
 }
